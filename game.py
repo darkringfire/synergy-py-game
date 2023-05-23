3caef3fbd1c17a618d4785ba1419763a9b32a736
darkringfire <darkringfire@gmail.com>
2023-05-23 08:36:54 +0800

    Refactor key scanning, add exit, pause, death

--- game.py
+++ game.py
@@ -7,14 +7,18 @@ from pynput import keyboard
 
 MAP_SIZE = 20, 10
 
+# TICK_DELAY = 0.05
 TICK_DELAY = 0.05
 
 map = Map(*MAP_SIZE)
 helicopter = Helicopter(map)
 
+stop_game = False
+pause_game = False
+
 
 def on_press(key):
-    try:
+    if type(key) == keyboard.KeyCode:
         if key.char == "w":
             helicopter.set_move_up()
         elif key.char == "a":
@@ -23,8 +27,13 @@ def on_press(key):
             helicopter.set_move_down()
         elif key.char == "d":
             helicopter.set_move_right()
-    except AttributeError:
-        pass
+    else:
+        if key == keyboard.Key.esc:
+            global stop_game
+            stop_game = True
+        elif key == keyboard.Key.space:
+            global pause_game
+            pause_game = not pause_game
 
 
 def on_release(key):
@@ -45,6 +54,25 @@ listener.start()
 tick = 0
 tick_time = 0
 while True:
+    if stop_game:
+        utils.cls()
+        print(f"Game stopped. Your score is {helicopter.score}")
+        break
+
+    if helicopter.is_dead():
+        utils.cls()
+        print(f"Game Over. Your score is {helicopter.score}")
+        print("[Esc] to quit")
+        time.sleep(0.5)
+        continue
+
+    if pause_game:
+        utils.cls()
+        print("Game paused.")
+        print("[Space] to continue [Esc] to quit")
+        time.sleep(0.5)
+        continue
+
     tick_time = time.time()
 
     map.process()
@@ -54,6 +82,7 @@ while True:
     screen += f"{tick * TICK_DELAY:.2f}s\n"
     screen += helicopter.print()
     screen += map.render()
+    screen += "[W/A/S/D] to move [Esc] to quit [Space] to pause\n"
     utils.cls()
     print(screen)
 
@@ -61,3 +90,5 @@ while True:
     if TICK_DELAY - tick_time > 0:
         time.sleep(TICK_DELAY - tick_time)
     tick += 1
+
+listener.stop()
